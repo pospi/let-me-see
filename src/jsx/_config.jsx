b236a7308b3915fac3a3853462fb1e041d8c566f
pospi <pospi@spadgos.com>
2015-01-29 23:16:36 +1000

    update ethereum and force HTTP api (others temporarily deprecated?)

--- src/jsx/_config.jsx
+++ src/jsx/_config.jsx
@@ -12,7 +12,7 @@
 mixin(ns, {
 	// If set, will force override the AutoProvider web3 transport type
 	'force_websocket_uri' : null,	// 'ws://localhost:40404/eth' is the default for the Go websocket server when run locally i.e. `ethereum -ws -loglevel=4`
-	'force_http_uri' : null,		// 'http://localhost:8080' is the default for the Go http server
+	'force_http_uri' : 'http://localhost:8080',		// 'http://localhost:8080' is the default for the Go http server
 });
 
 })(LetMeSee.config, LetMeSee.mixin);
